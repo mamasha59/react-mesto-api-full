b3fcaf62ed55d7bd06ce18cd98b163ee48d1698b
Алексей <68348736+mamasha59@users.noreply.github.com>
2021-08-23 13:42:38 +0300

    test

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
-// const cors = require('cors');
 const userRoutes = require('./routes/users');
 const cardRoutes = require('./routes/cards');
 const { createUser, login } = require('./controllers/users');
@@ -36,7 +35,6 @@ app.get('/crash-test', () => { // ---краш тест
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
-// app.use(cors());
 app.post('/signin', validationSignin, login); // ----авторизация
 app.post('/signup', validationSignUp, createUser);// ----регистрация
 
