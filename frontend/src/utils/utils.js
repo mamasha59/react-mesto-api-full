2f97ce69c4d544d57744e05f99ddcc90e9a33f6d
Алексей <68348736+mamasha59@users.noreply.github.com>
2021-08-25 22:51:54 +0300

    test

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,6 +1,6 @@
 // Объект настроек для экземпляров классов Api и Auth
 export const options = {
-  url: "https://api.future.bright.nomoredomains.club/",
+  url: "https://api.future.bright.nomoredomains.club",
   headers: {
     "Content-Type": "application/json",
   },
