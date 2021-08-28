6b6a4c4fd6dcc5ea285688f3fa679fcf2c505291
Алексей <68348736+mamasha59@users.noreply.github.com>
2021-08-28 18:21:04 +0300

    test

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,8 @@ const cardRoutes = require('./routes/cards');
 
 const NotFoundErr = require('./errors/not-found-err');
 
+const { createUser, login } = require('./controllers/users');
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -28,14 +30,11 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
 });
-const {
-  createUser,
-  login,
-} = require('./controllers/users');
 
 app.use(requestLogger);
 app.use(limiter);
@@ -44,24 +43,34 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const options = {
-  origin: [
-    'http://localhost:3000',
-    'https://future.bright.nomoredomains.club',
-    'http://future.bright.nomoredomains.club',
-    'https://api.future.bright.nomoredomains.club/',
-    'http://api.future.bright.nomoredomains.club',
-    'https://github.com/mamasha59/react-mesto-api-full',
+const allowedCors = [
+  'localhost:3000',
+  'https://future.bright.nomoredomains.club',
+  'http://future.bright.nomoredomains.club',
+  'https://api.future.bright.nomoredomains.club/',
+  'http://api.future.bright.nomoredomains.club',
+  'https://github.com/mamasha59/react-mesto-api-full',
+];
 
-  ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-  credentials: true,
-};
+app.use(cors());
 
-app.use('*', cors(options));
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  }
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+  }
+
+  next();
+});
 
 app.get('/crash-test', () => { // --краш тест
   setTimeout(() => {
